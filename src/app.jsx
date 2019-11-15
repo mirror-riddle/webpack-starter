7b67e535ac597e96284f61f648de57ff69bc7afd
hejin <hejinpy@163.com>
2019-11-15 10:28:23 +0800

    eslint support

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export const App = () => {
-  return (
-    <div className="app-container">
-      <div className="app-content">
-        <h2 className="title">Webpack Starter 1</h2>
-      </div>
+export const App = () => (
+  <div className="app-container">
+    <div className="app-content">
+      <h2 className="title">Webpack Starter 1</h2>
     </div>
-  );
-};
+  </div>
+);
