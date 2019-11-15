1d3754dae152eadabc704232456743630707f55f
hejin <hejinpy@163.com>
2019-11-15 16:03:14 +0800

    add prop-types

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const App = () => (
-  <div className="app-container">
-    <div className="app-content">
-      <h2 className="title">Webpack Starter 1</h2>
+const App = props => {
+  const { title } = props;
+  return (
+    <div className="app-container">
+      <div className="app-content">
+        <h2 className="title">{title}</h2>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+App.propTypes = {
+  title: PropTypes.string,
+};
+
+App.defaultProps = {
+  title: 'webpack starter',
+};
+
+export { App };
