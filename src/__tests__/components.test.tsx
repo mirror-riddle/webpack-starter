3da85f7f866db0bca80f5561c12072fd3e26b7a1
hejin <hejinpy@163.com>
2019-12-03 16:47:38 +0800

    update test

--- src/__tests__/components.test.tsx
+++ src/__tests__/components.test.tsx
@@ -4,12 +4,14 @@ import Renderer from 'react-test-renderer';
 import { GridItem } from 'components/interactive-grid/grid-item';
 import { RandomKey } from 'components/random-key';
 
-test('render <GridItem />', () => {
-  const json = Renderer.create(<GridItem className="cell" />).toJSON();
-  expect(json).toMatchSnapshot();
-});
+describe('test components', () => {
+  test('render <GridItem />', () => {
+    const json = Renderer.create(<GridItem className="cell" />).toJSON();
+    expect(json).toMatchSnapshot();
+  });
 
-test('render <RandomKey />', () => {
-  const json = Renderer.create(<RandomKey />).toJSON();
-  expect(json).toMatchSnapshot();
+  test('render <RandomKey />', () => {
+    const json = Renderer.create(<RandomKey />).toJSON();
+    expect(json).toMatchSnapshot();
+  });
 });
