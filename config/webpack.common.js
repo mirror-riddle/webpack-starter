74a570d00cbebaff8c8ba4ec1a552b3abd475f36
hejin <hejinpy@163.com>
2019-12-03 13:21:09 +0800

    move jest config to package.json

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,40 +25,62 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        include: paths.src,
-        use: [
-          'babel-loader',
+        oneOf: [
           {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        include: paths.src,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(css)$/,
-        exclude: /\.module\.css$/,
-        include: paths.src,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.module\.(css)$/,
-        include: paths.src,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]_[local]_[hash:base64:5]',
+            test: /\.(ts|tsx)$/,
+            include: paths.src,
+            use: [
+              'babel-loader',
+              {
+                loader: 'ts-loader',
+                options: {
+                  transpileOnly: true,
+                },
               },
+            ],
+          },
+          {
+            test: /\.(js|jsx)$/,
+            include: paths.src,
+            use: 'babel-loader',
+          },
+          {
+            test: /\.(css)$/,
+            exclude: /\.module\.css$/,
+            include: paths.src,
+            use: ['style-loader', 'css-loader'],
+          },
+          // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+          // using the extension .module.css
+          {
+            test: /\.module\.(css)$/,
+            include: paths.src,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    localIdentName: '[name]_[local]_[hash:base64:5]',
+                  },
+                },
+              },
+            ],
+          },
+          // "file" loader makes sure those assets get served by WebpackDevServer.
+          // When you `import` an asset, you get its (virtual) filename.
+          // In production, they would get copied to the `build` folder.
+          // This loader doesn't use a "test" so it will catch all modules
+          // that fall through the other loaders.
+          {
+            loader: require.resolve('file-loader'),
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise be processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
             },
           },
         ],
